206a94365f87fd87b8cca11f3eefc538418f64fd
RijalEka <rijaleka17@gmail.com>
2024-10-10 13:57:26 +0700

    KedaiNongs

---
+++ harga.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class PaymentSystem {
+    public void calculateChange(double total, double payment) {
+        if (payment >= total) {
+            double change = payment - total;
+            System.out.println("Kembalian: " + change);
+        } else {
+            System.out.println("Pembayaran kurang!");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Masukkan total harga:");
+        double total = sc.nextDouble();
+
+        System.out.println("Masukkan pembayaran:");
+        double payment = sc.nextDouble();
+
+        PaymentSystem ps = new PaymentSystem();
+        ps.calculateChange(total, payment);
+        
+    }
+}
